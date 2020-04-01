fc551d7910600f7bd7ed27ed118e0949c21ac723
baalajimaestro <baalajimaestro@pixelexperience.org>
2020-04-01 23:26:37 +0530

    Dependency updates and improvements
    Signed-off-by: baalajimaestro <baalajimaestro@pixelexperience.org>

--- gcc_builder.sh
+++ gcc_builder.sh
@@ -41,27 +41,20 @@ curl -sLo mpc.tar.gz https://ftp.gnu.org/gnu/mpc/mpc-1.1.0.tar.gz
 echo "Cloning ISL...."
 curl -sLo isl.tar.xz http://isl.gforge.inria.fr/isl-0.22.1.tar.xz
 
-for f in *.tar*; do tar xvf $f; done
-
-cd $GCC_PATH
-
-ln -s $SRC_PATH/gmp-6.2.0 gmp
-ln -s $SRC_PATH/mpc-1.1.0 mpc 
-ln -s $SRC_PATH/mpfr-4.0.2 mpfr 
-ln -s $SRC_PATH/isl-0.22.1 isl 
+for f in *.tar*; do tar xf $f; done
 
 export PATH=$GCC_OUTPUT_PATH/bin:$PATH
 
 echo "Building Binutils...."
 cd $SRC_PATH/binutils 
 mkdir /tmp/build-binutils && cd /tmp/build-binutils
-$SRC_PATH/binutils/configure --prefix=$GCC_OUTPUT_PATH --target=$TARGET --disable-multilib > /dev/null
-make -j8 &> /dev/null || make -j8
-make install-strip &> /dev/null || make install
+$SRC_PATH/binutils/configure --prefix=$GCC_OUTPUT_PATH --target=$TARGET --disable-multilib &> /dev/null
+make -j$(nproc) > /dev/null
+make install-strip > /dev/null
 
 echo "Building Linux Headers....."
 cd $SRC_PATH/linux
-make ARCH=arm64 INSTALL_HDR_PATH=$GCC_OUTPUT_PATH/$TARGET  headers_install &> /dev/null || make ARCH=arm64 INSTALL_HDR_PATH=$GCC_OUTPUT_PATH/$TARGET  headers_install
+make ARCH=arm64 INSTALL_HDR_PATH=$GCC_OUTPUT_PATH/$TARGET  headers_install &> /dev/null
 
 while true; do echo "Building Cross-Compiler in Progress....."; sleep 120; done &
 
@@ -96,13 +89,13 @@ $GCC_PATH/configure \
 --enable-plugins \
 --enable-languages=c \
 --prefix=$GCC_OUTPUT_PATH \
---with-gmp=$GCC_PATH/gmp \
---with-mpfr=$GCC_PATH/mpfr \
---with-mpc=$GCC_PATH/mpc \
---with-isl=$GCC_PATH/isl > /dev/null
+--with-gmp=$SRC_PATH/gmp-6.2.0 \
+--with-mpfr=$SRC_PATH/mpfr-4.0.2 \
+--with-mpc=$SRC_PATH/mpc-1.1.0 \
+--with-isl=$SRC_PATH/isl-0.22.1 > /dev/null
 
 echo "Building GCC Step-1...."
-make -j8 all-gcc > /dev/null
+make -j$(nproc) all-gcc > /dev/null
 make install-gcc > /dev/null
 echo "Built GCC...."
 
@@ -111,26 +104,26 @@ mkdir /tmp/build-glibc && cd /tmp/build-glibc
 echo "Building GLIBC Stage-1...."
 $SRC_PATH/glibc/configure \
 --prefix=$GCC_OUTPUT_PATH/$TARGET \
---build=$MACHTYPE \$(date +%d%m%y)
+--build=$MACHTYPE \
 --host=$TARGET \
 --target=$TARGET \
 --with-headers=$GCC_OUTPUT_PATH/$TARGET/include \
 --disable-multilib libc_cv_forced_unwind=yes > /dev/null
 
 make install-bootstrap-headers=yes install-headers > /dev/null
-make -j8 csu/subdir_lib > /dev/null
+make -j$(nproc) csu/subdir_lib > /dev/null
 install csu/crt1.o csu/crti.o csu/crtn.o $GCC_OUTPUT_PATH/$TARGET/lib > /dev/null
 $TARGET-gcc -nostdlib -nostartfiles -shared -x c /dev/null -o $GCC_OUTPUT_PATH/$TARGET/lib/libc.so > /dev/null
 touch $GCC_OUTPUT_PATH/$TARGET/include/gnu/stubs.h
 
 echo "Building GCC Stage-2...."
 cd /tmp/build-gcc 
-make -j8 all-target-libgcc > /dev/null
+make -j$(nproc) all-target-libgcc > /dev/null
 make install-target-libgcc > /dev/null
 
 echo "Building GCC Stage-3...."
 cd /tmp/build-glibc
-make -j8 > /dev/null
+make -j$(nproc) > /dev/null
 make install-strip > /dev/null
 
 jobs
